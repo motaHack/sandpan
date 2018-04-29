71c078e2c3fb30695902f721df944adcbb934f39
motaHack <firstacom_y9emo@icloud.com>
2018-04-29 18:50:44 +0900

    add get&head method test

--- test/serverTest.php
+++ test/serverTest.php
@@ -20,11 +20,41 @@ class serverTest extends TestCase
     $this->assertSame('text/html', $server->check_content_type('hi.php'));
   }
 
-  public function test_get ()
+  public function test_check_method()
   {
     $server = new TCPServer();
-    echo $server->check_content_type('contents/index.html');
-    $this->assertContains('hello world', $server->get('contents/index.html'));
+    $this->assertSame('GET', $server->check_method('GET / HTTP/1.1'));
+    $this->assertSame('HEAD', $server->check_method('HEAD / HTTP/1.1'));
   }
 
+  public function test_create_content_length()
+  {
+    $server = new TCPServer();
+    $body = file_get_contents('contents/index.html');
+    $this->assertSame(164, $server->create_content_length($body));
+  }
+
+  public function test_get()
+  {
+    $server = new TCPServer();
+    $line = "GET /index.html HTTP/1.1\r\n".
+            "Host: 127.0.0.1:8888\r\n".
+            "User-Agent: curl/7.54.0\r\n".
+            "Accept: */*  \r\n";
+    $this->assertContains('hello world', $server->get($line));
+  }
+  public function test_head()
+  {
+    $server = new TCPServer();
+    $line = "HEAD /index.html HTTP/1.1\r\n".
+            "Host: 127.0.0.1:8888\r\n".
+            "User-Agent: curl/7.54.0\r\n".
+            "Accept: */*  \r\n";
+    $this->assertNotContains('hello world', $server->head($line));
+  }
+
+  // public function test_head()
+  // {
+  //   $server = new TCPServer();
+  // }
 }
